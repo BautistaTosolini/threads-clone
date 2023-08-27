81d4be349c7c02eb9abf063ceaae93dba53ad0e1
Bautista <bautistatosolini@gmail.com>
2023-08-27 13:14:32 -0300

    Added individual thread page functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,40 @@
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs';
+
+import { fetchThreads } from '@/lib/actions/thread.actions';
+import ThreadCard from '@/components/cards/thread-card';
  
-export default function Home() {
+export default async function Home() {
+  const result = await fetchThreads({ pageNumber: 1, pageSize: 30 });
+  const user = await currentUser();
+
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.threads.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ): (
+          <>
+            {result.threads.map((thread) => {
+              return (
+                <ThreadCard 
+                  key={thread._id}
+                  id={thread._id}
+                  currentUserId={user?.id || ''}
+                  parentId={thread.parentId}
+                  content={thread.text}
+                  author={thread.author}
+                  community={thread.community}
+                  createdAt={thread.createdAt}
+                  comments={thread.children}
+                />
+              )
+            })}
+          </>
+        )}
+      </section>
     </>
   )
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -29,3 +29,77 @@ export async function createThread({ text, author, communityId, path }: CreateTh
   }
   
 };
+
+export async function fetchThreads({ pageNumber = 1, pageSize = 20 }) {
+  connectToDB();
+
+  // Calculate number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Fetch posts without parents
+  const threadsQuery = Thread.find({
+    parentId: {
+      $in: [null, undefined]
+    },
+  })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: 'author', 
+      model: User,
+    })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image',
+      },
+    })
+
+    const totalThreadsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+
+    const threads = await threadsQuery.exec();
+
+    const isNext = totalThreadsCount > skipAmount + threads.length;
+
+    return { threads, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+
+  try {
+    const thread = await Thread.findById(id)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image'
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id name parentId image'
+          },
+          {
+            path: 'children', 
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: '_id id name parentId image'
+            }
+          }
+        ]
+      })
+      .exec();
+
+      return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`)
+  }
+}
